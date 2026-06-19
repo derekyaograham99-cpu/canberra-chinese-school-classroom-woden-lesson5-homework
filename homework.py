13806fe73a728190a065f3977a60b9a87b84bbfb
Derek <derek.yao.graham99@gmail.com>
2026-06-19 14:08:03 +1000

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,4 +1,4 @@
-# Make a program that
+# Make a program that              #Dumbass wrote until they until#
 # -Keeps asking the user to enter a number until they until a zero
 # -Keeps track of the total sum.
 
@@ -8,12 +8,12 @@
 # and you can do it any way you like.)
 
 total = 0
-number = int(input("..."))
-while number != 0:
-  # do something
-
-print("Total = " + str(total))
-
+while True:
+    Number = int(input("Enter a number: "))
+    total = total + Number
+    print(total)
+    if Number == 0:
+        break
 
 
 
